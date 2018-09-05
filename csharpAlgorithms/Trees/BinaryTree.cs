b6657b5c88b3ef17158515366ce9b172a6dab48a
garrettwong <gwong005@gmail.com>
2018-09-04 23:07:35 -0700

    format trees

--- csharpAlgorithms/Trees/BinaryTree.cs
+++ csharpAlgorithms/Trees/BinaryTree.cs
@@ -4,30 +4,30 @@ using System.Linq;
 
 namespace csharpAlgorithms.Trees
 {
-	public class TreeNode
-	{
-		public int Value { get; set; }
-		public TreeNode Left { get; set; }
-		public TreeNode Right { get; set; }
-	}
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+    }
 
-	public class BinaryTree
-	{
-		public TreeNode Root { get; set; }
+    public class BinaryTree
+    {
+        public TreeNode Root { get; set; }
 
-		public BinaryTree(TreeNode root)
-		{
+        public BinaryTree(TreeNode root)
+        {
             this.Root = root;
-		}
+        }
 
-		public int FindMaxPathSum(TreeNode root)
-		{
-			if (root == null) return 0;
+        public int FindMaxPathSum(TreeNode root)
+        {
+            if (root == null) return 0;
 
-			int leftSum = FindMaxPathSum(root.Left);
-			int rightSum = FindMaxPathSum(root.Right);
+            int leftSum = FindMaxPathSum(root.Left);
+            int rightSum = FindMaxPathSum(root.Right);
 
-			return Math.Max(root.Value + leftSum, root.Value + rightSum);
-		}
-	}
+            return Math.Max(root.Value + leftSum, root.Value + rightSum);
+        }
+    }
 }
