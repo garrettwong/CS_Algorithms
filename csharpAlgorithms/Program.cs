1b68951d3248c998ca7c58ac93bf7e12478197ca
garrettwong <gwong005@gmail.com>
2018-09-04 23:04:51 -0700

    convert BinaryTree.cs to dotnetcore

--- csharpAlgorithms/Program.cs
+++ csharpAlgorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using csharpAlgorithms;
 
 namespace csharpAlgorithms
 {
---
+++ csharpAlgorithms/Trees/BinaryTree.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csharpAlgorithms.Trees
+{
+	public class TreeNode
+	{
+		public int Value { get; set; }
+		public TreeNode Left { get; set; }
+		public TreeNode Right { get; set; }
+	}
+
+	public class BinaryTree
+	{
+		public TreeNode Root { get; set; }
+
+		public BinaryTree(TreeNode root)
+		{
+            this.Root = root;
+		}
+
+		public int FindMaxPathSum(TreeNode root)
+		{
+			if (root == null) return 0;
+
+			int leftSum = FindMaxPathSum(root.Left);
+			int rightSum = FindMaxPathSum(root.Right);
+
+			return Math.Max(root.Value + leftSum, root.Value + rightSum);
+		}
+	}
+}
